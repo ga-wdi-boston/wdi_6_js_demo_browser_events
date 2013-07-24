97aab0bcbddf2f6d6b1582e7e46debfffdfb1a91
David Fisher <tibbon+github@gmail.com>
2013-07-24 11:33:20 -0400

    Class done

--- events.js
+++ events.js
@@ -1,10 +1,26 @@
+	// I'm going to write a function that changes the second table item to say "three"
+	// when I click on the table anywhere
+// This is global
+function modifyTableText() {
+	console.log(this);
+	document.getElementById("t2").innerHTML = "three";
+};
+
+
+
+// Things inside here are local to window.onload
 window.onload = function() {
+
+	var myTable = document.getElementById("outside-table");
+	myTable.addEventListener("click", modifyTableText);
+
+
 	// Tracking mouse position
-	document.getElementById("home").onmouseover = function(event){
-		console.log(event);
-		document.getElementById("x-coord").innerHTML = event.x;
-		document.getElementById("y-coord").innerHTML = event.y;
-	};
+	// document.getElementById("home").onmouseover = function(event){
+	// 	console.log(event);
+	// 	document.getElementById("x-coord").innerHTML = event.x;
+	// 	document.getElementById("y-coord").innerHTML = event.y;
+	// };
 
 
 	// We should only have ONE of any given id
@@ -14,7 +30,9 @@ window.onload = function() {
 	};
 	// It returns an array, so we need to select the first one
 	document.getElementsByTagName("h1")[0].onclick = function(event){
-		console.log("You clicked the H1");
+		// console.log(this);
+		// console.log("You clicked the H1");
+		this.innerHTML = "What is happening?"
 	};
 
 	// Selecting multiple H2's
@@ -40,24 +58,11 @@ window.onload = function() {
 
 	document.getElementById("danger-box").onmouseover = function(event) {
 		console.log(event);
-		document.getElementById("danger-box").innerHTML = "I'm sorry Dave. I can't let you open the hatch.";
+		this.innerHTML = "I'm sorry Dave. I can't let you open the hatch.";
 	};
 
 	document.getElementById("danger-box").onmouseout = function(event) {
 		console.log(event);
-		document.getElementById("danger-box").innerHTML = "It's ok. Just don't think about it again.";
+		this.innerHTML = "It's ok. Just don't think about it again.";
 	};
-
-
 };
-
-
-
-
-
-
-
-
-
-
-
