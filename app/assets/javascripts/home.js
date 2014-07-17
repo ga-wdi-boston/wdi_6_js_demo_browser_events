51bce0c182e57e4df8e9e3f6b89861214c534d3a
Alex Grant <grantovich@users.noreply.github.com>
2014-07-17 18:03:33 -0400

    In-class work committed

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,43 @@
-$(document).ready(function() {
-  // Code in here will run once the browser has loaded all the page HTML
-});
-
-// Create a "namespace" object so we only have a single global variable
 var DemoApp = {
-  testFunction: function() { return true; }
+  initialize: function() {
+    $('#click-me-button').click(function(event) {
+      alert('You did it!');
+      event.preventDefault();
+    });
+
+    $('#disorient').click(this.mirrorText);
+    $('#random-color-button, .color-changing').click(this.randomizeColor);
+    $('#word-form').submit(this.addFunnyWord);
+    $('#words-list').on('click', 'li', this.randomizeColor);
+    $('#randomize-words-button').click($.proxy(this.randomizeWords, this));
+  },
+
+  mirrorText: function() {
+    $(this).toggleClass('mirror');
+  },
+
+  randomizeColor: function(event) {
+    var newColor = Math.floor(Math.random() * 0xffffff).toString(16);
+    $(this).css('color', '#' + newColor);
+    if(event.preventDefault !== undefined){ event.preventDefault(); }
+  },
+
+  addFunnyWord: function(event) {
+    var newWord = $('#word-field').val();
+
+    if(newWord !== '') {
+      var newItem = $('<li>').text(newWord);
+      $('#words-list').append(newItem);
+      $('#word-field').val('');
+    }
+
+    event.preventDefault();
+  },
+
+  randomizeWords: function(event) {
+    $('#words-list li').each(this.randomizeColor);
+    event.preventDefault();
+  }
 };
+
+$(document).ready(function(){ DemoApp.initialize(); });
